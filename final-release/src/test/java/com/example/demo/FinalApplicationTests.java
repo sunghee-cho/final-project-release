ef563eafc6053a5386bf0f11cd0adc8cc7e32ae6
younseochoi <younseo42@gmail.com>
2022-11-03 13:04:36 +0900

    test 수정

--- final-release/src/test/java/com/example/demo/FinalApplicationTests.java
+++ final-release/src/test/java/com/example/demo/FinalApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 //@SpringBootTest
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FinalApplicationTests {
 
