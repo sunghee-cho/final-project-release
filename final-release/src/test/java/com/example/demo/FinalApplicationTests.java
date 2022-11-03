853b9ae4e14b71f8881434d33e572fc113b8290f
younseochoi <younseo42@gmail.com>
2022-11-03 11:50:10 +0900

    테스트 어노테이션 수정

--- final-release/src/test/java/com/example/demo/FinalApplicationTests.java
+++ final-release/src/test/java/com/example/demo/FinalApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FinalApplicationTests {
 
 	@Test
